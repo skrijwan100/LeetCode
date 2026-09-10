677e5cf36723027ac4fb37b506c3555e0e530dd8
skrijwan <skrijwan329@gmail.com>
2026-09-10 21:27:24 +0530

    add searchInsert function to find the index for target in sorted array

---
+++ SearchInsertPosition.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l=0; 
+        int h= nums.length-1;
+        int index=0;
+        while(l<=h){
+            int mid =l+(h-l)/2;
+            if(nums[mid]==target){
+               return mid;
+            }
+            if(nums[mid]>target){
+                h=mid-1;
+            }
+            if(nums[mid]<target){
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+}
